fb1f776395501ac8f9bddfbb6d8200656d31da06
cklewar@t-online.de <cklewar@t-online.de>
2022-09-28 11:55:40 +0200

    update example v3

--- versions.tf
+++ versions.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 1.3.0"
+  cloud {
+    organization = "cklewar"
+
+    workspaces {
+      name = "aws-ec2-module"
+    }
+  }
 
   required_providers {
     aws = {

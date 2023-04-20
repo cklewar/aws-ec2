aa81100d000fe379daea811480e4840c3f6c7187
cklewar <cklewar@t-online.de>
2023-04-20 13:52:20 +0200

    update modules

---
+++ versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.3.0"
+  cloud {
+    organization = "cklewar"
+
+    workspaces {
+      name = "aws-ec2-module"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.59.0"
+    }
+
+    local = ">= 2.2.3"
+    null  = ">= 3.1.1"
+  }
+}

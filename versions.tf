d6d1b9d1b751838b8c0140ff197f76317b96bd13
cklewar <cklewar@t-online.de>
2023-11-07 17:36:45 +0100

    Launch 0.11.23

--- versions.tf
+++ versions.tf
@@ -2,19 +2,19 @@ terraform {
   required_version = ">= 1.3.0"
   cloud {
     organization = "cklewar"
+    hostname     = "app.terraform.io"
 
     workspaces {
       name = "aws-ec2-module"
     }
   }
-
+  
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = ">= 4.59.0"
     }
-
     local = ">= 2.2.3"
-    null  = ">= 3.1.1"
+    null = ">= 3.1.1"
   }
 }
